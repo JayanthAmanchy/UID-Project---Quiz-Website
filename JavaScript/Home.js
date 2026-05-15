06159c7a6580f14cfb0e77f033746aa9deb5f2f7
Jayanth Amanchy <jayanthamanchy369@gmail.com>
2026-05-15 22:52:45 +0530

    Add files via upload

---
+++ JavaScript/Home.js
@@ -0,0 +1,18 @@
+let science_selected = false;
+let sports_selected = false;
+let tech_selected = false;
+let maths_selected = false;
+let selected = false;
+
+    function science() {
+        window.location.href = "Science.html";
+    }
+    function maths() {
+        window.location.href = "Maths.html";
+    }
+    function generalknowledge() {
+        window.location.href = "GK.html";
+    }
+    function tech() {
+        window.location.href = "Technology.html";
+    }
