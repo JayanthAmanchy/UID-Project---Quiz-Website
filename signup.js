2b7c1bc94389ba31cc470e91d1f21b34fce4b3f2
Jayanth Amanchy <jayanthamanchy369@gmail.com>
2026-05-16 10:36:04 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,29 @@
+function showMessage(event) {
+
+    event.preventDefault();
+
+    let firstname = document.getElementById("firstName").value;
+    let lastname = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    if(password.length < 8) {
+        alert("Password must be at least 8 characters long.");
+        return;
+    }
+
+    document.getElementById("signup").innerHTML = `
+
+        <h2>Sign Up Successful!</h2>
+        <p>Welcome ${firstname}</p>
+
+        <button onclick="goToLogin()">
+            Go To Login
+        </button>
+
+    `;
+    
+}
+
+function goToLogin() {
+    window.location.href = "login.html";
+}
