29cdd080390daaf5d615af2873a16d1037afc88c
Jayanth Amanchy <jayanthamanchy369@gmail.com>
2026-05-16 07:34:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function science(){
+    openPage("Science.html");
+}
+
+function maths(){
+    openPage("Maths.html");
+}
+
+function tech(){
+    openPage("Technology.html");
+}
+
+function generalknowledgek(){
+    openPage("GK.html");
+}
+
+function openPage(page){
+    window.location.href = page;
+}
